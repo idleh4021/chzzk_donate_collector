cab8e5da02bead611854f45022eebd3dff0a0631
이동현 <idleh4021@naver.com>
2026-02-22 01:01:04 +0900

    pyinstaller command 주석

--- run_app.py
+++ run_app.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 import platform
 
+#uv run pyinstaller --onefile --icon="backend/assets/icon.ico" --add-data "frontend/dist;frontend/dist" --add-data "backend/assets;backend/assets" backend/main.py
+
 def run():
     # 1. 경로 설정
     root_dir = os.getcwd()
